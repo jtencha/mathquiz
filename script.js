68d5b54c7c49e0c970518e359616b9446b334c6c
PrancingPony <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-08-03 14:00:03 -0500

    move to js

---
+++ script.js
@@ -0,0 +1,17 @@
+function check_style_status() {
+	var solu = document.getElementById("solution").value;
+	var answer = document.getElementById("user-answer").value;
+	var proceed = document.getElementById("submit_button");
+	let text;
+	if (solu == answer) {
+	  text = 'Nice work!';
+	} else {
+		text = 'Not quite... the correct answer was:';
+    document.getElementById("sol").innerHTML = solu;
+	}
+	// Lock submit button to prevent submitting twice
+  document.getElementById("check_answer").className="disappear";
+	document.getElementById("submit_button").className="appear";
+// Return result to page
+  document.getElementById("feedback").innerHTML = text;
+}
