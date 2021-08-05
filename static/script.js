43f668a19982b29a138c042c65091c47809e0c73
PrancingPony <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-08-05 18:26:51 -0500

    Custom Feedback!

--- static/script.js
+++ static/script.js
@@ -1,13 +1,18 @@
 function check_style_status() {
-	var solu = document.getElementById("solution").value;
+	var solution = document.getElementById("solution").value;
 	var answer = document.getElementById("user-answer").value;
 	var proceed = document.getElementById("submit_button");
 	let text;
-	if (solu == answer) {
-	  text = 'Nice work!';
+	if (solution == answer) {
+		var responses = [ "Nice Work!", "Awesome!", "Excellent!", "Great Job!"];
+		var picking = Math.floor(Math.random() * responses.length);
+  	var final = responses[picking];
+	  text = final;
 	} else {
-		text = 'Not quite... the correct answer was:';
-    document.getElementById("sol").innerHTML = solu;
+		var responses = ["Almost! ", "Not quite! ", "Oof! ", "*Disappointing Trombone Music* "]
+		var picking = Math.floor(Math.random() * responses.length);
+		var final = responses[picking];
+    text = final + "The correct answer was " + solution + ".";
 	}
 	// Lock submit button to prevent submitting twice
   document.getElementById("check_answer").className="disappear";
