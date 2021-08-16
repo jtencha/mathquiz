767eddbe594ccc9afc766d79f37f7302ee60ac36
PrancingPony <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-08-16 14:04:17 -0500

    add time

--- static/script.js
+++ static/script.js
@@ -1,9 +1,21 @@
+var onLoad = new Date();
+function timer() {
+  return (new Date() - onLoad) / 1000;
+}
+
 function check_style_status() {
+	var time = timer();
+	var cap = document.getElementById("time_per").value;
 	var solution = document.getElementById("solution").value;
-	var answer = document.getElementById("user-answer").value;
+	var answer = document.getElementById("user_answer").value;
 	var proceed = document.getElementById("submit_button");
 	let text;
-	if (solution == answer) {
+	if (time > cap) {
+		//set the answer as none so the python reads it as incorrect
+		document.doquiz.user_answer.value = "";
+		text = "You exceeded your " + cap + " second limit!";
+	}
+	else if (solution == answer) {
 		var responses = [ "Nice Work!", "Awesome!", "Excellent!", "Great Job!"];
 		var picking = Math.floor(Math.random() * responses.length);
   	var final = responses[picking];
@@ -14,10 +26,10 @@ function check_style_status() {
 		var final = responses[picking];
     text = final + "The correct answer was " + solution + ".";
 	}
-	// Lock submit button to prevent submitting twice
-  document.getElementById("check_answer").className="disappear";
-	document.getElementById("submit_button").className="appear";
-	document.getElementById('user-answer').readOnly = true;
+// Lock submit button to prevent submitting twice
+document.getElementById("check_answer").className="disappear";
+document.getElementById("submit_button").className="appear";
+document.getElementById('user_answer').readOnly = true;
 // Return result to page
-  document.getElementById("feedback").innerHTML = text;
+document.getElementById("feedback").innerHTML = text;
 }
